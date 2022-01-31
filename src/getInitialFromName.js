03190f9a776a390785dd06be070dafe1d74758cd
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-31 18:55:01 +0100

    Added functionality for getInitialFromName

--- src/getInitialFromName.js
+++ src/getInitialFromName.js
@@ -1,5 +1,19 @@
 const getInitialFromName = (name) => {
-  return "F";
+  const prefixes = ["el", "la", "de", "the"];
+
+  const destructuredName = name.split(" ");
+
+  if (destructuredName.length === 1) {
+    console.log(name[0]);
+    return name[0];
+  }
+
+  if (prefixes.some((prefix) => prefix === destructuredName[0].toLowerCase())) {
+    console.log(destructuredName[0]);
+    return destructuredName[1][0];
+  } else {
+    return destructuredName[0][0];
+  }
 };
 
 export default getInitialFromName;
