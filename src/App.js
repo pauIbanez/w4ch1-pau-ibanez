49e622ad699b4db83b4911d080641e4c1d181821
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-31 18:45:15 +0100

    Made correct info injection and changes class to className

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import gentlemen from "./data/gentlemen";
 
 function App() {
   return (
-    <div class="container">
-      <header class="main-header">
-        <h1 class="main-title">The pointing gentlemen</h1>
+    <div className="container">
+      <header className="main-header">
+        <h1 className="main-title">The pointing gentlemen</h1>
       </header>
-      <section class="controls">
-        <p class="info">0 gentlemen pointing at you</p>
-        <button class="button button--select">Select all</button>
+      <section className="controls">
+        <p className="info">0 gentlemen pointing at you</p>
+        <button className="button button--select">Select all</button>
       </section>
-      <main class="main">
-        <ul class="gentlemen">
+      <main className="main">
+        <ul className="gentlemen">
           {gentlemen.map((gentleman) => {
-            return <Gentleman manInfo={gentleman} />;
+            return <Gentleman key={gentleman.id} manInfo={gentleman} />;
           })}
         </ul>
       </main>
--- src/components/Gentleman/Gentleman.js
+++ src/components/Gentleman/Gentleman.js
@@ -1,39 +1,33 @@
 const Gentleman = ({
-  name,
-  profession,
-  status,
-  twitter,
-  image,
-  initial,
-  onClick,
+  manInfo: { name, profession, status, twitter, image, initial, onClick },
 }) => {
   return (
-    <li class="gentleman" onClick={onClick}>
-      <div class="gentleman__avatar-container">
+    <li className="gentleman" onClick={onClick}>
+      <div className="gentleman__avatar-container">
         <img
-          class="gentleman__avatar"
+          className="gentleman__avatar"
           src={image}
           alt="The Fary pointing at you"
         />
-        <span class="gentleman__initial">{initial}</span>
+        <span className="gentleman__initial">{initial}</span>
       </div>
-      <div class="gentleman__data-container">
-        <h2 class="gentleman__name">{name}</h2>
-        <ul class="gentleman__data-list">
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Profession:</span>
+      <div className="gentleman__data-container">
+        <h2 className="gentleman__name">{name}</h2>
+        <ul className="gentleman__data-list">
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Profession:</span>
             {profession}
           </li>
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Status:</span> {status}
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Status:</span> {status}
           </li>
-          <li class="gentleman__data">
-            <span class="gentleman__data-label">Twitter:</span> {twitter}
+          <li className="gentleman__data">
+            <span className="gentleman__data-label">Twitter:</span> {twitter}
           </li>
         </ul>
       </div>
-      <i class="icon gentleman__icon fas fa-check"></i>
-      <i class="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
+      <i className="icon gentleman__icon fas fa-check"></i>
+      <i className="icon gentleman__icon gentleman__icon--delete fas fa-times"></i>
     </li>
   );
 };
