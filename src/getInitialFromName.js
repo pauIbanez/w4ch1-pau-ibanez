8ef7639ab3a28cea83adc89bdfb40b8ac7157947
Pau Ibáñez <pauibanez2001@gmail.com>
2022-01-31 18:55:42 +0100

    Removed missed console.log

--- src/getInitialFromName.js
+++ src/getInitialFromName.js
@@ -4,12 +4,10 @@ const getInitialFromName = (name) => {
   const destructuredName = name.split(" ");
 
   if (destructuredName.length === 1) {
-    console.log(name[0]);
     return name[0];
   }
 
   if (prefixes.some((prefix) => prefix === destructuredName[0].toLowerCase())) {
-    console.log(destructuredName[0]);
     return destructuredName[1][0];
   } else {
     return destructuredName[0][0];
